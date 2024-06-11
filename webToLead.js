7594b1f9c85ff6cf94b61553aa42846f5608a391
neha-nandanwar9 <nehanandanwar.sfdc@gmail.com>
2024-06-11 18:10:01 +0530

    first commit

---
+++ webToLead.js
@@ -0,0 +1,12 @@
+console.log('Web to Lead')
+
+let inputDate = document.querySelector('.inputDate')
+let outputDate = document.querySelector('.outputDate')
+
+beforeSubmit = () => {
+    console.log('Input Value: ', inputDate.value);
+    let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN")
+    outputDate.value = formattedDate;
+}
+
+beforeSubmit();
