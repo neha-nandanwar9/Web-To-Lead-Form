4677a98ef93cf893ad359b6f7c0c30d2f0b9a6fa
neha-nandanwar9 <nehanandanwar.sfdc@gmail.com>
2024-06-14 09:53:09 +0530

    JS & Html edited

--- webToLead.js
+++ webToLead.js
@@ -4,7 +4,7 @@ let inputDate = document.querySelector('.inputDate')
 let outputDate = document.querySelector('.outputDate')
 let captchaChecked = false;
 beforeSubmit = (event) => {
-    if(captchaChecked=true){
+    if(captchaChecked){
     console.log('Input Value: ', inputDate.value);
     let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN")
     outputDate.value = formattedDate;
@@ -14,14 +14,14 @@ beforeSubmit = (event) => {
     }
 }
 
-beforeSubmit(); 
 
 function timestamp() { 
     var response = document.getElementById("g-recaptcha-response"); 
     if (response == null || response.value.trim() == "") 
         {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
             elems["ts"] = JSON.stringify(new Date().getTime());
-            document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+            document.getElementsByName("captcha_settings")[0].value = 
+            JSON.stringify(elems); 
         }
      } setInterval(timestamp, 500); 
 
